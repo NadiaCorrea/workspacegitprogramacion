07b5271916dddd0f4813cdeae31e4c28f748a025
NadiaCorrea <ncormor536@gmail.com>
2022-03-24 22:37:08 +0100

    commit ejercicio 1 done

--- collections/src/com/jacaranda/equipo/Equipo.java
+++ collections/src/com/jacaranda/equipo/Equipo.java
@@ -17,7 +17,7 @@ public class Equipo {
 	}
 
 	public void addAlumno(Alumno alumno) throws EquipoException {
-		if (listado.contains(alumno)) {// si el listado contine el alumno lanza error
+		if (listado.contains(alumno)) {// si el listado contiene el alumno lanza error
 			throw new EquipoException("Alumno ya existe.");
 		} else {
 			listado.add(alumno);
@@ -26,7 +26,7 @@ public class Equipo {
 	}
 
 	public void addAlumno2(Alumno alumno) throws EquipoException {
-		if (!listado.add(alumno) || alumno == null) {// si el listado contine el alumno lanza error
+		if (alumno == null || !listado.add(alumno)) {// si el listado contiene el alumno lanza error
 			throw new EquipoException("Alumno ya existe o nulo.");
 		}
 
@@ -69,28 +69,51 @@ public class Equipo {
 	public String mostrarListaPersonas() {
 		StringBuilder result = new StringBuilder();
 
-		for (Alumno a : listado) {
-			result.append("Alumno " + a + "\n");
+		for (Alumno a : this.listado) {
+			result.append(a + "\n");
 		}
 		return result.toString();
 
 	}
 
-	public Equipo unionEquipo(Equipo e2) throws EquipoException {
-//iterator
+	// Unión de equipos. El método lo llamará un equipo y
+	// se le pasará por parámetro el otro equipo con el que se quiere unir.
+	// Devuelve un equipo formado por los alumnos de ambos equipos.
 
+	public Equipo unionEquipo(Equipo e2) throws EquipoException {
 		Equipo result = new Equipo("");
+
 		for (Alumno a : this.listado) {
-			if (e2.listado.contains(a)) {// pon un try and catch
+			result.addAlumno(a);
+		}
+
+		for (Alumno a : e2.listado) {
+			if (result.listado.contains(a) == false) {
 				result.addAlumno(a);
 			}
 		}
 		return result;
 	}
 
+//	Intersección de equipos.  Idem al anterior pero 
+//	devuelve un equipo formado por los alumnos que están en los dos equipos 
+
+	public Equipo interseccionEquipo(Equipo e2) throws EquipoException {
+		Equipo result = new Equipo("");
+
+		for (Alumno a : this.listado) {
+			if (e2.listado.contains(a) == true) {
+				result.addAlumno(a);
+			}
+		}
+
+		return result;
+
+	}
+
 	@Override
 	public String toString() {
-		return "Equipo [nombre=" + nombre + ", listado=" + listado + "]";
+		return "Equipo - " + nombre + ", Listado de alumnos: " + listado;
 	}
 
 	public int numAlumnos() {
--- collections/src/com/jacaranda/principal/Principal.java
+++ collections/src/com/jacaranda/principal/Principal.java
@@ -12,34 +12,80 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Alumno a = new Alumno("inma", "44444");
-
-		Equipo e = new Equipo("1ÂºDAW-B");
+		Equipo e1 = new Equipo("1ºDAW-B");
+		Equipo e2 = new Equipo("1ºDAW-A");
 
 		Set<Equipo> liga = new HashSet<Equipo>();
 
-		liga.add(e);
+		liga.add(e1);
 
 		try {
-			e.addAlumno(a);
-			System.out.println("Alumno aÃ±adido");
-			Alumno a2 = new Alumno("inma", "4444");
-			e.addAlumno(a2);
-			System.out.println("Alumno aÃ±adido");
-			System.out.println(e.numAlumnos());
+			Alumno a = new Alumno("Inma", "44444");
+			e1.addAlumno(a);
+			System.out.println("Alumno añadido");
+
+			Alumno a2 = new Alumno("Nadia", "4445");
+			e1.addAlumno(a2);
+			System.out.println("Alumno añadido");
+
+			Alumno a3 = new Alumno("Pedro", "4446");
+			e1.addAlumno(a3);
+			System.out.println("Alumno añadido");
+
+			Alumno a4 = new Alumno("Paco", "4447");
+			e1.addAlumno(a4);
+			System.out.println("Alumno añadido");
+
+			Alumno a5 = new Alumno("Claudia", "4448");
+			e1.addAlumno(a5);
+			System.out.println("Alumno añadido");
+
+			Alumno a6 = new Alumno("Juana", "4452");
+			e1.addAlumno(a6);
+			System.out.println("Alumno añadido");
+
+			System.out.println(e1.numAlumnos());
 //			e.addAlumno(null);
 //			e.addAlumno(null);
 //			System.out.println(e.numAlumnos());
 //			System.out.println(e);
 
-			e.borrarAlumno(a);
-			System.out.println(e.numAlumnos());
-			System.out.println(e.buscarAlumno(a));
-			System.out.println(e.buscarAlumno(a2));
+			e2.addAlumno(a);
+			System.out.println("Alumno añadido");
 
-		} catch (EquipoException e2) {
+			e2.addAlumno(a5);
+			System.out.println("Alumno añadido");
+
+			Alumno a7 = new Alumno("Inmanol", "44449");
+			e2.addAlumno(a7);
+			System.out.println("Alumno añadido");
+
+			Alumno a8 = new Alumno("Sandra", "44450");
+			e2.addAlumno(a8);
+			System.out.println("Alumno añadido");
+
+			Alumno a9 = new Alumno("Paul", "44451");
+			e2.addAlumno(a9);
+			System.out.println("Alumno añadido");
+
+			e1.borrarAlumno(a3);
+			System.out.println("Alumno eliminado.");
+
+			System.out.println("El equipo 1 tiene " + e1.numAlumnos() + " alumnos.");
+			System.out.println("El equipo 2 tiene " + e2.numAlumnos() + " alumnos.");
+
+			System.out.println(e1.buscarAlumno(a));
+			System.out.println(e2.buscarAlumno(a8));
+
+			System.out.println("Equipo 1:\n" + e1.mostrarListaPersonas());
+			System.out.println("Equipo 2:\n" + e2.mostrarListaPersonas());
+
+			System.out.println("La unión de equipos: " + e1.unionEquipo(e2));
+			System.out.println("La intersección de equipos: " + e2.interseccionEquipo(e1));
+
+		} catch (EquipoException e5) {
 			// TODO: handle exception
-			System.out.println(e2.getMessage());
+			System.out.println(e5.getMessage());
 		}
 
 	}
