de245813fa1a67a162b4e4d8d59acc1cb29bba88
NadiaCorrea <ncormor536@gmail.com>
2022-04-03 22:00:44 +0200

    commit prueba de bucles done

--- entorno/src/boletin/OrdenarNum.java
+++ entorno/src/boletin/OrdenarNum.java
@@ -1,40 +1,65 @@
 package boletin;
 
+import java.util.Arrays;
+
 public class OrdenarNum {
 
 	public static void main(String[] args) {
 		/*
-		 * Realizar un programa que ordene un array de “n” números enteros. Usar el
-		 * método de ordenación por selección
+		 * Realizar un programa que ordene un array de n números enteros. Usar el método
+		 * de ordenación por selección
 		 */
 		int[] numeros = { 1, 2, 9, 3, 4, 5, 8, 10, 6, 7 };
+		ordenarNum(numeros);
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println(numeros[i]);
+		}
 
-//		System.out.println(OrdenarNum(numeros));
+		int[] caso1 = { 1 };
+		ordenarNum(caso1);
+		System.out.println(Arrays.toString(caso1));
+
+		int[] caso2 = { 9, 1 };
+		ordenarNum(caso2);
+		System.out.println(Arrays.toString(caso2));
+
+		int[] caso3 = { 9, 1, 0 };
+		ordenarNum(caso3);
+		System.out.println(Arrays.toString(caso3));
+
+		int[] caso4 = { 9, 1, 0, 3, 7, 10 };
+		ordenarNum(caso4);
+		System.out.println(Arrays.toString(caso4));
+
+		int[] caso5 = { 1, 2, 9, 3, 4, 5, 8, 10, 6, 7 };
+		ordenarNum(caso5);
+		System.out.println(Arrays.toString(caso5));
+
 	}
 
-//	public static int[] odenarNum(int[] num) {
-//
-//		for (int i = 0; i < num.length - 1; i = i + 1) {
-//			int min = i;
-//			for (int j = i + 1; j < num.length; j = j + 1) {
-//				if (num[j] < num[min]) {
-//					min = j;
-//				}
-//			}
-//			
-//
-//		}
+	public static void ordenarNum(int[] num) {
+		int i, j;
+		int menor;
+		int pos;
+		int numAux;
+
+		for (i = 0; i < num.length - 1; i = i + 1) {
+			menor = num[i];
+			pos = i;
+			for (j = i + 1; j < num.length; j = j + 1) {
+				if (num[j] < menor) {
+					menor = num[j];
+					pos = j;
+				}
+			}
+			if (pos != i) {
+				numAux = num[i];
+				num[i] = num[pos];
+				num[pos] = numAux;
+			}
+
+		}
 
 	}
-
-//	for(int i = 0; i < n-1 ; i++){
-//	      int minimo = i;
-//	      for(int j = i + 1 ; j < n ; j++){
-//	            if(lista[j] < lista[minimo]){
-//	                  minimo = j;
-//	            }
-//	      }
-//	      intercambiar(lista , i , minimo);
-//	}
 
 }
